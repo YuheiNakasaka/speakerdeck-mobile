dea985dabbc94ae3282684b887c35ed4222c813e
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2018-05-18 22:58:49 +0900

    Add credit to footer

---
+++ components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="footer">
+    <div>
+      <a href="https://github.com/YuheiNakasaka/speakerdeck-mobile" target="_blank" rel="noopener">
+        Written by YuheiNakasaka
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+#footer {
+  width: 100%;
+  text-align: center;
+  a {
+    font-size: 12px;
+    font-weight: bold;
+    color: #fff;
+    text-decoration: none;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,15 +2,17 @@
   <div>
     <Header></Header>
     <HomeForm></HomeForm>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
 import Header from "~/components/Header";
 import HomeForm from "~/components/HomeForm";
+import Footer from "~/components/Footer";
 
 export default {
-  components: {Header, HomeForm}
+  components: {Header, HomeForm, Footer}
 }
 </script>
 
