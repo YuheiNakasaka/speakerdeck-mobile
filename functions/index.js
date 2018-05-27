ccf0684316f4ec3af453a60c638b9ee1d3c250a3
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2018-05-27 17:57:33 +0900

    はてぶはSSRでhead作成しないとダメだったので元に戻す

--- functions/index.js
+++ functions/index.js
@@ -14,16 +14,17 @@ const nuxt = new Nuxt({
 
 function handleRequest(req, res) {
     const agent = parser(req.headers['user-agent']);
-    if (!req.url.match(/\/$|\?.+/) && agent.device.type !== "mobile") {
+    if ((!req.url.match(/\/$|\?.+/) && agent.device.type !== "mobile")) {
       res.redirect(`https://speakerdeck.com${req.url}`);
+      return;
+    } else {
+      res.set('Cache-Control', 'public, max-age=600, s-maxage=1200');
+      return new Promise((resolve, reject) => {
+          nuxt.render(req, res, (promise) => {
+              promise.then(resolve).catch(reject);
+          });
+      });
     }
-
-    res.set('Cache-Control', 'public, max-age=600, s-maxage=1200');
-    return new Promise((resolve, reject) => {
-        nuxt.render(req, res, (promise) => {
-            promise.then(resolve).catch(reject);
-        });
-    });
 }
 
 app.use(handleRequest);
